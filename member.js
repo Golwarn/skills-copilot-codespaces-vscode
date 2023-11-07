83b827a211bbc95e68a275a49ab81a41d58707c2
Thomas Krause <thomaskrausemail@gmail.com>
2023-11-07 15:57:46 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,1 +1,15 @@
-function skillsMember()
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'templates/skills-member.html',
+    scope: {
+      member: '='
+    },
+    controller: function($scope) {
+      $scope.showSkills = false;
+      $scope.toggleSkills = function() {
+        $scope.showSkills = !$scope.showSkills;
+      };
+    }
+  };
+}
